1a6e382bd194074dfea08de17cb6c6ab4598085a
Jarle Nygård <jarle@famnygard.com>
2015-01-17 14:32:59 +0100

    Working on getting to table

---
+++ WebAPI.Backend/Models/IncidentDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+
+namespace WebAPI.Backend.Models
+{
+    public class IncidentDto : TableEntity
+    {
+        public IncidentDto()
+        {
+        }
+
+        public IncidentDto(string company, string id)
+        {
+            PartitionKey = company;
+            RowKey = id;
+        }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("status")]
+        public string Status { get; set; }
+        [JsonProperty("deadline")]
+        public DateTime Deadline { get; set; }
+    }
+}
---
+++ WebAPI.Backend/Models/IncidentRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace WebAPI.Backend.Models
+{
+    public class IncidentRepository
+    {
+        private StorageCredentials cred = new StorageCredentials("uxhurdle",
+            "FdLMeczIrFo7Jtcoad1GL6AFYS5abqU2G53T3KapK/pbS1lRzNMq7jZdz8ziZy2ScewEzRn66w0/NnkzbZehzA==");
+
+        StorageUri uri = new StorageUri(new Uri("https://uxhurdle.table.core.windows.net/"));
+
+        public IncidentRepository(CloudTableClient client)
+        {
+            if (client == null)
+                client = new CloudTableClient(uri, cred);
+
+            var table = client.GetTableReference("incident");
+        }
+    }
+}
