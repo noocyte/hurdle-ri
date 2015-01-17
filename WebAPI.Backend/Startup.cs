45563e3e370803e07f738ab231ca15ee866b6084
Jarle Nygård <jarle@famnygard.com>
2015-01-17 14:14:30 +0100

    Removed crap
    No security, just bare-metal API packages + deployment

---
+++ WebAPI.Backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof (WebAPI.Backend.Startup))]
+
+namespace WebAPI.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
