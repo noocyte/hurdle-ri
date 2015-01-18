d32a6d50271e01f4c3e741e4ea9fa7ebdbd41b50
Jarle Nygård <jarle@famnygard.com>
2015-01-18 13:20:57 +0100

    Adding files...

---
+++ WebAPI.Backend/Models/TableResponse.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace WebAPI.Backend.Models
+{
+    public class TableResponse<T> where T : TableEntity
+    {
+        private readonly TableResult _result;
+
+        public TableResponse(TableResult result)
+        {
+            _result = result;
+        }
+
+        public bool Success { get { return _result.HttpStatusCode < 300; }} // 304?
+        public HttpStatusCode Status { get { return (HttpStatusCode)_result.HttpStatusCode; } }
+        public T Result
+        {
+            get
+            {
+                if (Success)
+                    return _result.Result as T;
+                return default(T);
+            }
+        }
+    }
+}
